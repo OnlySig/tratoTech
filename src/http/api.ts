dfc6bb623c2720b06832dd838818fd1b9f06dc82
Gabriel Castro <72272327+OnlySig@users.noreply.github.com>
2024-07-01 14:51:44 -0300

    Update api.ts

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://trato-tech-roan.vercel.app/'
+    baseURL: 'https://trato-tech-roan.vercel.app/db.json'
 })
 
 export default instance
